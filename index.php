efcabf43cefda0c76d13e9372f89c2e6164f4a32
John Spruijt <99058314@mydavinci.nl>
2019-12-11 10:57:56 +0100

    removed macosX files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!--John Spruijt 2019-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>B3W21O1</title>
+</head>
+<body>
+
+    <?php
+    $time = dateTime();
+    if ($time =<6:00 ) {
+        print "$time"
+    };
+    ?>
+</body>
+</html>
